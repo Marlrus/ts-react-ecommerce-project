7d0b0fd9031115f27379af0eee2b36877d42aada
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-05-14 16:17:45 -0500

    Created shop page and collection preview component

---
+++ src/pages/shop/shop.component.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import SHOP_DATA from './shop.data.js'
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+
+interface ShopPageState {
+	collections: {
+		id: number;
+		title: string;
+		routeName: string;
+		items: {
+			id: number;
+			name: string;
+			imageUrl: string;
+			price: number;
+		}[];
+	}[]
+}
+
+
+class ShopPage extends React.Component <any, ShopPageState> {
+   constructor(props: any){
+      super(props)
+
+      this.state = {
+			collections: SHOP_DATA
+		}
+		
+	}
+	
+	render() {
+		const { collections } = this.state
+		return (<div className="shop-page">
+			{
+				collections.map(({id, ...otherCollectionProps}) => (
+					<CollectionPreview key={id} {...otherCollectionProps } />
+				))
+			}
+		</div>)
+	}
+}
+
+export default ShopPage
