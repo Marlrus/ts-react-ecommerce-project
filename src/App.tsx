a04f1fb8911e28e7efe3912405a5425a5b988818
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-05-18 15:54:25 -0500

    Created SignIn Component, added it to page and styled to have bot forms appear at once, enabled email/password in Firebase and effectivelly created new local user in db

--- src/App.tsx
+++ src/App.tsx
@@ -36,12 +36,15 @@ class App extends React.Component<any, AppState> {
                );
 
                userRef!.onSnapshot((snapShot) => {
-                  this.setState({
-                     currentUser: {
-                        id: snapShot.id,
-                        ...snapShot.data(),
+                  this.setState(
+                     {
+                        currentUser: {
+                           id: snapShot.id,
+                           ...snapShot.data(),
+                        },
                      },
-                  });
+                     () => console.log(this.state)
+                  );
                });
             } else {
                this.setState({ currentUser: userAuth });
