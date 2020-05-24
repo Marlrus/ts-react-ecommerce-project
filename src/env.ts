41e9168efd8b14737d175d04285ae660eb57ea40
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-05-23 19:12:57 -0500

    Heroku Build .env removed from gitignore

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+export const FIREBASE_CONFIG = {
+   apiKey: "AIzaSyBhGNwfno-QsrSGsMrFA77tzCHVFOFCjpE",
+   authDomain: "crwn-db-ts.firebaseapp.com",
+   databaseURL: "https://crwn-db-ts.firebaseio.com",
+   projectId: "crwn-db-ts",
+   storageBucket: "crwn-db-ts.appspot.com",
+   messagingSenderId: "256556511989",
+   appId: "1:256556511989:web:24901739a4ba1728d70bbc",
+   measurementId: "G-JV6S2PR4YD"
+};
