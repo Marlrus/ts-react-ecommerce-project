e8e8bd75a3bfef12c31d6f6099425d610ad8f67b
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-05-25 15:47:31 -0500

    Added styled-components, refactored HomePage and Header Components

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect, ConnectedProps } from 'react-redux';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
@@ -10,35 +9,34 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import './header.styles.scss';
+import {
+   HeaderContaner,
+   LogoContainer,
+   OptionsContainer,
+   OptionLink,
+} from './header.styles';
 
 import { State } from '../../redux/store.types';
 
 const Header: React.FC<HeaderProps> = ({ currentUser, hidden }) => (
-   <div className='header'>
-      <Link className='logo-container' to='/'>
+   <HeaderContaner>
+      <LogoContainer to='/'>
          <Logo className='logo' />
-      </Link>
-      <div className='options'>
-         <Link className='option' to='/shop'>
-            SHOP
-         </Link>
-         <Link className='option' to='/contact'>
-            CONTACT
-         </Link>
+      </LogoContainer>
+      <OptionsContainer>
+         <OptionLink to='/shop'>SHOP</OptionLink>
+         <OptionLink to='/contact'>CONTACT</OptionLink>
          {currentUser ? (
-            <div className='option' onClick={() => auth.signOut()}>
+            <OptionLink as='div' onClick={() => auth.signOut()}>
                SIGN OUT
-            </div>
+            </OptionLink>
          ) : (
-            <Link className='option' to='/signin'>
-               SIGN IN
-            </Link>
+            <OptionLink to='/signin'>SIGN IN</OptionLink>
          )}
          <CartIcon />
-      </div>
+      </OptionsContainer>
       {hidden ? null : <CartDropdown />}
-   </div>
+   </HeaderContaner>
 );
 
 const mapStateToProps = (state: State) => ({
