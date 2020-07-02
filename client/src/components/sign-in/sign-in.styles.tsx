79282e19d659df7174eab733d34c9a944a7dc79f
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-07-02 17:21:34 -0500

    Mobile support for sign in and sign up

--- client/src/components/sign-in/sign-in.styles.tsx
+++ client/src/components/sign-in/sign-in.styles.tsx
@@ -4,6 +4,10 @@ export const SignInContainer = styled.div`
    width: 380px;
    display: flex;
    flex-direction: column;
+
+   @media screen and (max-width: 800px) {
+      width: 320px;
+   }
 `;
 
 export const SignInTitle = styled.h2`
@@ -13,4 +17,9 @@ export const SignInTitle = styled.h2`
 export const ButtonContainer = styled.div`
    display: flex;
    justify-content: space-between;
+
+   @media screen and (max-width: 800px) {
+      display: grid;
+      grid-layout: 1fr;
+   }
 `;
--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.style.tsx
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.style.tsx
@@ -5,4 +5,10 @@ export const SignInAndSignUpContainer = styled.div`
    display: flex;
    justify-content: space-between;
    margin: 30px auto;
+
+   @media screen and (max-width: 800px) {
+      width: 95%;
+      display: grid;
+      grid-layout: 1fr;
+   }
 `;
