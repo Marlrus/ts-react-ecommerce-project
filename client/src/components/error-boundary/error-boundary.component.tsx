40dc068a2b81d6a739714cc1234b918cbf537271
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-07-10 13:23:10 -0500

    Improved style to ErrorBoundary

--- client/src/components/error-boundary/error-boundary.component.tsx
+++ client/src/components/error-boundary/error-boundary.component.tsx
@@ -1,7 +1,14 @@
 import React, { ErrorInfo } from 'react';
 
+import {
+   ErrorImageContainer,
+   ErrorImageOverlay,
+   ErrorImageText,
+} from './error-boundary.styles';
+
 interface ErrorBoundaryState {
    hasErrored: boolean;
+   errMessage?: string;
 }
 
 class ErrorBoundary extends React.Component<any, ErrorBoundaryState> {
@@ -15,7 +22,7 @@ class ErrorBoundary extends React.Component<any, ErrorBoundaryState> {
 
    static getDerivedStateFromError(err: Error) {
       //process err
-      return { hasErrored: true };
+      return { hasErrored: true, errMessage: err.message };
    }
 
    componentDidCatch(err: Error, errInfo: ErrorInfo) {
@@ -23,8 +30,16 @@ class ErrorBoundary extends React.Component<any, ErrorBoundaryState> {
    }
 
    render() {
+      const { errMessage } = this.state;
       return this.state.hasErrored ? (
-         <div>Something went wrong</div>
+         <ErrorImageOverlay>
+            <ErrorImageContainer imageUrl='https://i.imgur.com/yW2W9SC.png' />
+            <ErrorImageText>
+               Sorry, something went wrong with the page uWu.
+               <br />
+               {errMessage ? <span>Error: {errMessage}</span> : null}
+            </ErrorImageText>
+         </ErrorImageOverlay>
       ) : (
          this.props.children
       );
